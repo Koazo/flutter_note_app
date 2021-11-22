03663e7be2669b534613e4e3d4788a966542156a
Mansur <dasaimq@gmail.com>
2021-11-22 22:05:56 +0300

    generated hive's TypeAdapter<NoteModel>

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
+part 'note_model.g.dart';
+
+@HiveType(typeId: 1)
 class NoteModel {
+  @HiveField(0)
   int? id;
+  @HiveField(1)
   String? title;
+  @HiveField(2)
   String? content;
+  @HiveField(3)
   DateTime? createdDate;
+  @HiveField(4)
   Color? color;
 
   NoteModel({
---
+++ lib/models/note_model.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'note_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class NoteModelAdapter extends TypeAdapter<NoteModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  NoteModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NoteModel(
+      id: fields[0] as int?,
+      title: fields[1] as String?,
+      content: fields[2] as String?,
+      createdDate: fields[3] as DateTime?,
+      color: fields[4] as Color?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, NoteModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.content)
+      ..writeByte(3)
+      ..write(obj.createdDate)
+      ..writeByte(4)
+      ..write(obj.color);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NoteModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
